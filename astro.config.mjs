3a49f0e629be43077ff592e5d5c70f2e21b0cff3
MIFSH <Z18538383923@gmail.com>
2024-06-03 18:59:12 +0800

    dfgsdfvsd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,29 @@
-import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marxchou.com',
   markdown: {
-    syntaxHighlight: 'prism',
+    syntaxHighlight: 'prism'
   },
-  integrations: [sitemap(), icon()],
+  integrations: [sitemap(), icon(), mdx()],
   image: {
     // 示例：通过自定义配置启用基于 Sharp 的图像服务
     service: {
       entrypoint: 'astro/assets/services/sharp',
       config: {
-        limitInputPixels: false,
-      },
-    },
+        limitInputPixels: false
+      }
+    }
   },
   domains: ['avatars.githubusercontent.com'],
-  remotePatterns: [
-    {
-      protocol: 'https',
-    },
-  ],
+  remotePatterns: [{
+    protocol: 'https'
+  }],
   trailingSlash: 'always',
-  output: 'static',
-})
+  output: 'static'
+});
