cd01bbd33a18f0059a3583cbea9183e36d309596
MIFSH <Z18538383923@gmail.com>
2024-06-27 05:35:22 +0800

    gggh

--- src/integrations/redirect.ts
+++ src/integrations/redirect.ts
@@ -5,6 +5,7 @@ import fg from "fast-glob";
 import { minify } from "html-minifier";
 import { JSDOM } from "jsdom";
 import type { AstroIntegration } from "astro";
+import { config } from "../self.config.ts";
 
 const redirectIntegration = (): AstroIntegration => ({
     name: "redirectAttributeByLink",
@@ -25,14 +26,14 @@ const redirectIntegration = (): AstroIntegration => ({
 
                     try {
                         // 是否启用跳转功能
-                        const enable = true; // 设为 true 以启用跳转功能
+                        const enable = config.redirect; // 设为 true 以启用跳转功能
                         if (!enable) return;
                         // 中转页地址
                         const redirectPage = "/redirect/?url=";
                         // 包含的 className
-                        const includeClass = ["post__content"];
+                        const includeClass = config.redirectIncludeClass;
                         // 排除的 className
-                        const excludeClass = [];
+                        const excludeClass = config.redirectExcludeClass;
 
                         // 判断元素或其父元素是否包含指定类名
                         const hasIncludeClass = (element) => {
@@ -96,6 +97,18 @@ const redirectIntegration = (): AstroIntegration => ({
                                     link.setAttribute("href", redirectLink);
                                     // 增加 target="_blank" 属性
                                     link.setAttribute("target", "_blank");
+                                    // 创建并插入 SVG 图标
+                                    if (hasIncludeClass(link)) {
+                                        const svgIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                                        svgIcon.setAttribute("width", "16");
+                                        svgIcon.setAttribute("height", "16");
+                                        svgIcon.setAttribute("viewBox", "0 0 24 24");
+                                        svgIcon.setAttribute("fill", "currentColor");
+                                        svgIcon.innerHTML = `
+                                            <path fill="currentColor" d="M12.75 17.5a.75.75 0 0 0 0-1.5H6.5a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h6.25a.75.75 0 0 0 0-1.5H6.5A3.5 3.5 0 0 0 3 6v8a3.5 3.5 0 0 0 3.5 3.5zm.991-11.301a.75.75 0 0 1 1.06.042l3 3.25a.75.75 0 0 1 0 1.018l-3 3.25A.75.75 0 1 1 13.7 12.74l1.838-1.991H7.75a.75.75 0 0 1 0-1.5h7.787l-1.838-1.991a.75.75 0 0 1 .042-1.06"/>
+                                        `;
+                                        link.appendChild(svgIcon);
+                                    }
                                 }
                             }
                         });
