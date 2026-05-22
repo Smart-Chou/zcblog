63cb5446f3f20aa7bb60077e7fe102a0f436fd54
MIFSH <Z18538383923@gmail.com>
2026-05-22 21:39:01 +0800

    feat: 添加打印样式，优化打印输出格式和内容 fix: 更新标签颜色获取函数，使用等级代替颜色值 fix: 修改媒体卡背景颜色为 CSS 变量 fix: 更新标签索引内容，移除不必要的注释 fix: 为文章集合的标签字段设置默认值

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,7 +15,7 @@ const article = defineCollection({
             summary: z.string().nullable().optional(),
             pubDate: z.date(),
             upDate: z.date().nullable().optional(),
-            tags: z.array(z.string()),
+            tags: z.array(z.string()).default([]),
             image: z
                 .object({
                     url: z.preprocess(
